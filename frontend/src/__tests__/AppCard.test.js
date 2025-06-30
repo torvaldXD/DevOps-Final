17d6a67bc62031df2dcb43d2bda067279a9ff1ed
M.B. Nuñez Zuñiga <103372738+MarvinB97@users.noreply.github.com>
2025-06-30 11:37:50 -0500

    Update AppCard.test.js
    act

--- frontend/src/__tests__/AppCard.test.js
+++ frontend/src/__tests__/AppCard.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import { act } from 'react';
+//import { act } from 'react';
 import AppCard from '../components/AppCard';
 import axios from 'axios';
 
